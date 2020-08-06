0c2d34ff766c75f8ddfaad68200156a2f7e0b636
Dan Lawrence <delawr0190@gmail.com>
2020-08-06 10:19:01 -0400

    Initial project structure

---
+++ src/test/java/mn/foreman/windowsagent/WindowsAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package mn.foreman.windowsagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowsAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
